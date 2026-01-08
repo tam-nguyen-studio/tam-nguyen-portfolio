90373e7926b27f6b3bd9cd3a27c2835fd98db3a5
Tam Nguyen <tamguyen@Tams-MBP.lan>
2026-01-07 19:55:52 -0500

    Update site content

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SERVICES } from '../constants';
 
-const RESUME_URL = '/images/tam-nguyen-resume.pdf';
+const RESUME_URL = '/images/tam-nguyen-resum.pdf';
 
 interface AboutProps {}
 
